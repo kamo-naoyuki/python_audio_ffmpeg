487826e2baa4bb3f1c48376373250346fe647bbe
kamo-naoyuki <naoyuki.kamo829@gmail.com>
2018-01-20 11:16:42 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,9 @@ from setuptools import setup
 setup(name='audio_ffmpeg',
       version='1.0',
       packages=['audio_ffmpeg'],
+      description=['Python ffmpeg wrapper to convert '
+                   'from numpy.ndarray to numpy.ndarray'],
+      url='https://github.com/kamo-naoyuki/python_audio_ffmpeg/edit/master/setup.py',
       author='Naoyuki Kamo',
       install_requires=['numpy']
       )
